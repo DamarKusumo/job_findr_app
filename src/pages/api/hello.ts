d1e7b5a96c8894692bff4f23ad7df831bf1b0d9e
Damar-Kusumo <muhammad.damar01@ui.ac.id>
2024-05-01 21:53:16 +0700

    Test Selenium

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { helloSelenium } from "@/scraper/saveData"
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const data = helloSelenium();
+  console.log(data);
+  res.status(200).json({ name: "John Doe" });
+}
