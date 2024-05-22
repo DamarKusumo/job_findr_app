e876d46bd6231842629702025e58911f75cacb4f
Damar-Kusumo <muhammad.damar01@ui.ac.id>
2024-05-22 14:27:54 +0700

    Update Date Filter

--- src/utils/firebaseSetting.js
+++ src/utils/firebaseSetting.js
@@ -66,7 +66,7 @@ export const filterData = async (collection, position, pubDate, location, compan
     let query = db.collection(collection);
 
     let conditions = [
-        pubDate ? ["publicationDate", "<", new Date(pubDate)] : null,
+        pubDate ? ["publicationDate", ">", new Date(pubDate)] : null,
         location ? ["location", "==", location] : null,
         company ? ["companyIdx", "array-contains", (company).toLowerCase()] : null,
     ].filter(Boolean);
